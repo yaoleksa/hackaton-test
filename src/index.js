3ec41f4a23e9a953982ade0b1bc451893c05345d
yaoleksa <mryaremchyk@gmail.com>
2023-05-16 18:26:59 +0300

    add button controller

--- src/index.js
+++ src/index.js
@@ -3,10 +3,43 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
+import Form from 'react-bootstrap/Form';
 
 const ie = [{date: new Date(), source: 'work', amount: '1200'}];
-const button = (innerText, v) => {
-    return (<Button variant={v} className="btn">{innerText}</Button>);
+const hide = () => {
+    let form = document.getElementById('form');
+    if(form.style.display == 'none'){
+        form.style.display = '';
+    } else {
+        form.style.display = 'none';
+    }
+}
+const inputForm = () => {
+    return (
+        <Form id="form" style={{display: 'none'}}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    );
+}
+const button = (innerText, v, f) => {
+    return (<Button variant={v} className="btn" onClick={f}>{innerText}</Button>);
 }
 const incomeTable = () => {
     return (<>
@@ -32,7 +65,8 @@ const incomeTable = () => {
 }
 const App = () => {
     return (<>{incomeTable()}
-    {button("add income", "success")}{button("add expense", "danger")}</>);
+    {button("add income", "success", hide)}{button("add expense", "danger", null)}
+    {inputForm()}</>);
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
