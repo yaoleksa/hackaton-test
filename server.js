aee624f60a59c20a794ee38604217bca561e1f85
yaoleksa <mryaremchyk@gmail.com>
2023-05-16 11:58:01 +0300

    add database

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:', err => {
+    if(err){
+        return 'Failed to connect';
+    } else {
+        return 'Successfuly connected to database';
+    }
+});
+db.serialize(() => {
+    db.run('CREATE TABLE IF NOT EXISTS Profit (Date TEXT, Source TEXT, Amount REAL)');
+});
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -11,3 +22,4 @@ app.get('/', (req,res) => {
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 });
+db.close();
