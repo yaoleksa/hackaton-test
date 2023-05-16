b4c7175764388dfda92ac0e9176b343a99b49dde
yaoleksa <mryaremchyk@gmail.com>
2023-05-16 23:52:04 +0300

    add button controller

--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ const inputForm = () => {
     return (
         <Form id="form" style={{display: 'none'}}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Label>Transaction date</Form.Label>
+                <Form.Control type="datetime-local" placeholder="Enter email" />
                 <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
                 </Form.Text>
